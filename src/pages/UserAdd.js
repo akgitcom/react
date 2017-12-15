6e3970f1a77c9ec9ffd20e447960178878b0c635
aksoft <aksoft@163.com>
2017-12-15 13:20:25 +0800

    添加表单验证

--- src/pages/UserAdd.js
+++ src/pages/UserAdd.js
@@ -4,37 +4,118 @@ class UserAdd extends React.Component {
     constructor() {
         super();
         this.state = {
-            name: '',
-            email: '',
-            password:'',
-            department_id:1,
-            status: -1
+            form: {
+                name: {
+                    valid: false,
+                    value: '',
+                    error: ''
+                },
+                email: {
+                    valid: false,
+                    value: '',
+                    error: ''
+                },
+                password: {
+                    valid: false,
+                    value: '',
+                    error: ''
+                },
+                department_id: {
+                    valid: false,
+                    value: 1,
+                    error: ''
+                },
+                status: {
+                    valid: false,
+                    value: -1,
+                    error: ''
+                }
+            }
         };
     }
     handleValueChange(field, value, type = 'string') {
-        // 由于表单的值都是字符串，我们可以根据传入type为number来手动转换value的类型为number类型
         if (type === 'number') {
             value = +value;
         }
 
+        const { form } = this.state;
+
+        const newFieldObj = { value, valid: true, error: '' };
+
+        switch (field) {
+            case 'name': {
+                if (value.length >= 7) {
+                    newFieldObj.error = '用户名最多6个字符';
+                    newFieldObj.valid = false;
+                } else if (value.length === 0) {
+                    newFieldObj.error = '请输入用户名';
+                    newFieldObj.valid = false;
+                }
+                break;
+            }
+            case 'email': {
+                if (!/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(value)) {
+                    newFieldObj.error = '请输入正确的邮箱地址';
+                    newFieldObj.valid = false;
+                } else if (value.length === 0) {
+                    newFieldObj.error = '请输入邮箱';
+                    newFieldObj.valid = false;
+                }
+                break;
+            }
+            case 'password': {
+                if (value.length >= 7) {
+                    newFieldObj.error = '密码名最多6个字符';
+                    newFieldObj.valid = false;
+                } else if (value.length === 0) {
+                    newFieldObj.error = '请输入密码';
+                    newFieldObj.valid = false;
+                }
+                break;
+            }
+            case 'department_id': {
+                if (value > 5 || value <= 0) {
+                    newFieldObj.error = '请输入选择1-5之间的数字';
+                    newFieldObj.valid = false;
+                }
+                break;
+            }
+            case 'status': {
+                if (value < 0) {
+                    newFieldObj.error = '请选择状态';
+                    newFieldObj.valid = false;
+                }
+                break;
+            }
+        }
+
         this.setState({
-            [field]: value
+            form: {
+                ...form,
+                [field]: newFieldObj
+            }
         });
     }
     handleSubmit(e) {
         e.preventDefault();
 
-        const { name, email, password, department_id, status } = this.state;
+        const { form: { name, email, password, department_id, status } } = this.state;
+        //在handleSubmit方法中对每个字段的valid进行检测，如果有一个valid为false则直接return以中断提交操作。
+        if (!name.valid || !email.valid || !password.valid || !department_id.valid || !status.valid) {
+            alert('请填写正确的信息后重试');
+            return;
+        }
+
         fetch('http://localhost:8080/api/user', {
-            mode: "cors",  
+            mode: "cors",
             method: 'POST',
             // 使用fetch提交的json数据需要使用JSON.stringify转换为字符串
             body: JSON.stringify({
-                name,
-                email,
-                password,
-                department_id,
-                status
+                name: name.value,
+                email: email.value,
+                password: password.value,
+                department_id: department_id.value,
+                status: status.value
             }),
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
@@ -50,7 +131,7 @@ class UserAdd extends React.Component {
                         name: '',
                         email: '',
                         password: '',
-                        department_id:0,
+                        department_id: 0,
                         status: -1
                     });
                 } else {
@@ -60,7 +141,7 @@ class UserAdd extends React.Component {
             .catch((err) => console.error(err));
     }
     render() {
-        const { name, email,password, department_id, status } = this.state;
+        const { form: { name, email, password, department_id, status } } = this.state;
         return (
             <div>
                 <header>
@@ -70,25 +151,28 @@ class UserAdd extends React.Component {
                 <main>
                     <form onSubmit={(e) => this.handleSubmit(e)}>
                         <label>用户名：</label>
-                        <input type="text" value={name} onChange={(e) => this.handleValueChange('name', e.target.value)} />
+                        <input type="text" value={name.value} onChange={(e) => this.handleValueChange('name', e.target.value)} />
+                        {!name.valid && <span>{name.error}</span>}
                         <br />
                         <label>邮箱：</label>
-                        <input type="text" value={email} onChange={(e) => this.handleValueChange('email', e.target.value)} />
+                        <input type="email" value={email.value} onChange={(e) => this.handleValueChange('email', e.target.value)} />
+                        {!email.valid && <span>{email.error}</span>}
                         <br />
                         <label>密码：</label>
-                        <input type="password" value={password || ''} onChange={(e) => this.handleValueChange('password', e.target.value)} />
-                        <br />
+                        <input type="password" value={password.value || ''} onChange={(e) => this.handleValueChange('password', e.target.value)} />
+                        {!password.valid && <span>{password.error}</span>}
                         <br />
                         <label>部门：</label>
-                        <input type="number" value={department_id || ''} onChange={(e) => this.handleValueChange('department_id', e.target.value, 'number')} />
+                        <input type="number" value={department_id.value || ''} onChange={(e) => this.handleValueChange('department_id', e.target.value, 'number')} />
+                        {!department_id.valid && <span>{department_id.error}</span>}
                         <br />
                         <label>状态：</label>
-                        <select value={status} onChange={(e) => this.handleValueChange('status', e.target.value, 'number')}>
+                        <select value={status.value} onChange={(e) => this.handleValueChange('status', e.target.value, 'number')}>
                             <option value="-1">请选择</option>
                             <option value="0">开启</option>
                             <option value="1">关闭</option>
                         </select>
-                        <br />
+                        {!status.valid && <span>{status.error}</span>}
                         <br />
                         <input type="submit" value="提交" />
                     </form>
