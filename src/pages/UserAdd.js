47a9f86ffcfb6bb01c07812ccdf247ffa8ec5d39
aksoft <aksoft@163.com>
2017-12-15 17:33:39 +0800

    组件化表单/表单控件

--- src/pages/UserAdd.js
+++ src/pages/UserAdd.js
@@ -1,107 +1,13 @@
 import React from 'react';
-
+import FormItem from "../components/FormItem";
+import formProvider from '../utils/formProvider';
 class UserAdd extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            form: {
-                name: {
-                    valid: false,
-                    value: '',
-                    error: ''
-                },
-                email: {
-                    valid: false,
-                    value: '',
-                    error: ''
-                },
-                password: {
-                    valid: false,
-                    value: '',
-                    error: ''
-                },
-                department_id: {
-                    valid: false,
-                    value: '',
-                    error: ''
-                },
-                status: {
-                    valid: false,
-                    value: -1,
-                    error: ''
-                }
-            }
-        };
-    }
-    handleValueChange(field, value, type = 'string') {
-        if (type === 'number') {
-            value = +value;
-        }
-
-        const { form } = this.state;
-
-        const newFieldObj = { value, valid: true, error: '' };
-
-        switch (field) {
-            case 'name': {
-                if (value.length >= 7) {
-                    newFieldObj.error = '用户名最多6个字符';
-                    newFieldObj.valid = false;
-                } else if (value.length === 0) {
-                    newFieldObj.error = '请输入用户名';
-                    newFieldObj.valid = false;
-                }
-                break;
-            }
-            case 'email': {
-                if (!/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(value)) {
-                    newFieldObj.error = '请输入正确的邮箱地址';
-                    newFieldObj.valid = false;
-                } else if (value.length === 0) {
-                    newFieldObj.error = '请输入邮箱';
-                    newFieldObj.valid = false;
-                }
-                break;
-            }
-            case 'password': {
-                if (value.length >= 7) {
-                    newFieldObj.error = '密码名最多6个字符';
-                    newFieldObj.valid = false;
-                } else if (value.length === 0) {
-                    newFieldObj.error = '请输入密码';
-                    newFieldObj.valid = false;
-                }
-                break;
-            }
-            case 'department_id': {
-                if (value > 5 || value <= 0) {
-                    newFieldObj.error = '请输入选择1-5之间的数字';
-                    newFieldObj.valid = false;
-                }
-                break;
-            }
-            case 'status': {
-                if (value < 0) {
-                    newFieldObj.error = '请选择状态';
-                    newFieldObj.valid = false;
-                }
-                break;
-            }
-        }
-
-        this.setState({
-            form: {
-                ...form,
-                [field]: newFieldObj
-            }
-        });
-    }
     handleSubmit(e) {
         e.preventDefault();
 
-        const { form: { name, email, password, department_id, status } } = this.state;
+        const { form: { name, email, password, department_id, status }, formValid } = this.props;
         //在handleSubmit方法中对每个字段的valid进行检测，如果有一个valid为false则直接return以中断提交操作。
-        if (!name.valid || !email.valid || !password.valid || !department_id.valid || !status.valid) {
+        if (!formValid) {
             alert('请填写正确的信息后重试');
             return;
         }
@@ -127,13 +33,6 @@ class UserAdd extends React.Component {
                 // 所以可以使用res.id来判断添加是否成功
                 if (res.id) {
                     alert('添加用户成功');
-                    this.setState({
-                        name: '',
-                        email: '',
-                        password: '',
-                        department_id: '',
-                        status: -1
-                    });
                 } else {
                     alert('添加失败');
                 }
@@ -141,7 +40,7 @@ class UserAdd extends React.Component {
             .catch((err) => console.error(err));
     }
     render() {
-        const { form: { name, email, password, department_id, status } } = this.state;
+        const { form: { name, email, password, department_id, status }, onFormChange } = this.props;
         return (
             <div>
                 <header>
@@ -150,30 +49,44 @@ class UserAdd extends React.Component {
 
                 <main>
                     <form onSubmit={(e) => this.handleSubmit(e)}>
-                        <label>用户名：</label>
-                        <input type="text" value={name.value} onChange={(e) => this.handleValueChange('name', e.target.value)} />
-                        {!name.valid && <span>{name.error}</span>}
-                        <br />
-                        <label>邮箱：</label>
-                        <input type="email" value={email.value} onChange={(e) => this.handleValueChange('email', e.target.value)} />
-                        {!email.valid && <span>{email.error}</span>}
-                        <br />
-                        <label>密码：</label>
-                        <input type="password" value={password.value || ''} onChange={(e) => this.handleValueChange('password', e.target.value)} />
-                        {!password.valid && <span>{password.error}</span>}
-                        <br />
-                        <label>部门：</label>
-                        <input type="number" value={department_id.value || ''} onChange={(e) => this.handleValueChange('department_id', e.target.value, 'number')} />
-                        {!department_id.valid && <span>{department_id.error}</span>}
-                        <br />
-                        <label>状态：</label>
-                        <select value={status.value} onChange={(e) => this.handleValueChange('status', e.target.value, 'number')}>
-                            <option value="-1">请选择</option>
-                            <option value="0">开启</option>
-                            <option value="1">关闭</option>
-                        </select>
-                        {!status.valid && <span>{status.error}</span>}
-                        <br />
+                        <FormItem label="用户名：" valid={name.valid} error={name.error}>
+                            <input
+                                type="text"
+                                value={name.value}
+                                onChange={(e) => onFormChange('name', e.target.value)}
+                            />
+                        </FormItem>
+                        <FormItem label="邮箱：" valid={email.valid} error={email.error}>
+                            <input
+                                type="text"
+                                value={email.value}
+                                onChange={(e) => onFormChange('email', e.target.value)}
+                            />
+                        </FormItem>
+                        <FormItem label="密码：" valid={password.valid} error={password.error}>
+                            <input
+                                type="text"
+                                value={password.value}
+                                onChange={(e) => onFormChange('password', e.target.value)}
+                            />
+                        </FormItem>
+                        <FormItem label="部门：" valid={department_id.valid} error={department_id.error}>
+                            <input
+                                type="number"
+                                value={department_id.value}
+                                onChange={(e) => onFormChange('department_id', e.target.value)}
+                            />
+                        </FormItem>
+                        <FormItem label="状态：" valid={status.valid} error={status.error}>
+                            <select
+                                value={status.value}
+                                onChange={(e) => onFormChange('status', e.target.value)}
+                            >
+                                <option value="-1">请选择</option>
+                                <option value="0">开启</option>
+                                <option value="1">禁用</option>
+                            </select>
+                        </FormItem>
                         <input type="submit" value="提交" />
                     </form>
                 </main>
@@ -181,5 +94,74 @@ class UserAdd extends React.Component {
         );
     }
 }
+UserAdd = formProvider({
+    name: {
+        defaultValue: '',
+        rules: [
+            {
+                pattern: function (value) {
+                    return value.length > 0;
+                },
+                error: '请输入用户名'
+            },
+            {
+                pattern: /^.{1,8}$/,
+                error: '用户名最多8个字符'
+            }
+        ]
+    },
+    email: {
+        defaultValue: '',
+        rules: [
+            {
+                pattern: function (value) {
+                    return value.length > 0;
+                },
+                error: '请输入邮箱地址'
+            },
+            {
+                pattern: /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/,
+                error: '请输入正确的邮箱地址'
+            }
+        ]
+    },
+    password: {
+        defaultValue: '',
+        rules: [
+            {
+                pattern: function (value) {
+                    return value.length > 0;
+                },
+                error: '请输入密码'
+            },
+            {
+                pattern: /^.{1,8}$/,
+                error: '密码最多8个字符'
+            }
+        ]
+    },
+    department_id: {
+        defaultValue: '',
+        rules: [
+            {
+                pattern: function (value) {
+                    return value > 0 && value < 5;
+                },
+                error: '请选择部门'
+            }
+        ]
+    },
+    status: {
+        defaultValue: '',
+        rules: [
+            {
+                pattern: function (value) {
+                    return value >= 0 && value <2;
+                },
+                error: '请选择状态'
+            }
+        ]
+    }
+})(UserAdd);
 
 export default UserAdd;
